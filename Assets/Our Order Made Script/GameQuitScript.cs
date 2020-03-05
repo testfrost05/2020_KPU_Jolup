fc00086fa9e8b6ddcb1100a7e926943e9622cf76
2016180050KHJ <ashe01@naver.com>
2020-03-05 12:04:46 +0900

    Start & Quit button Implement

---
+++ Assets/Our Order Made Script/GameQuitScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameQuitScript : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Button Click");
+    }
+}
