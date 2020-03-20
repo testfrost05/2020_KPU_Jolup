4bc9565827b8106338daa3190140f45a3062bf15
Lee Hyo Jun <u7i333@naver.com>
2020-03-20 16:21:56 +0900

    VR FPS

---
+++ Assets/Our Order Made Script/VR FPS/VRFPS Core/Slot.cs
@@ -0,0 +1,222 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VrFps
+{
+    public class Slot : MonoBehaviour //아이템 넣을 수 있는 슬롯
+    {
+        [SerializeField] protected Transform offset;
+        public virtual Transform Offset
+        {
+            get
+            {
+                return offset;
+            }
+        }
+
+        protected Rigidbody offsetRigidbody;
+
+        public Rigidbody OffsetRigidbody
+        {
+            get
+            {
+                return offsetRigidbody;
+            }
+        }
+
+        [SerializeField] public int sizeLimit; //들고 다닐수 있는 크기 제한
+        [SerializeField] protected List<string> AcceptedTags = new List<string>();
+
+        [SerializeField] protected bool poseItem;
+
+        [HideInInspector] public List<Item> stackedItems = new List<Item>();
+        public bool StackIsFull
+        {
+            get
+            { return stackedItems.Count >= 5;
+            }
+        }
+        public bool StackIsEmpty
+        {
+            get
+            {
+                return stackedItems.Count == 0;
+            }
+        }
+
+        [ReadOnly] [SerializeField] protected Item storedItem;
+
+        public virtual Item StoredItem //아이템 저장
+        {
+            get
+            {
+                return storedItem;
+            }
+            set
+            {
+                storedItem = value; }
+        }
+
+
+        private void Start()
+        {
+            gameObject.tag = "Slot";
+            gameObject.layer = LayerMask.NameToLayer("ItemDetection"); //레이어 설정
+        }
+
+        public bool HasItem //아이템을 가지고 있는지
+        {
+            get
+            {
+                return storedItem;
+            }
+        }
+
+        public bool StackableStoredItem
+        {
+            get
+            {
+                return storedItem ? storedItem.Stackable : false;
+            }
+        }
+
+        public void UnstoreItemLight(Item item)
+        {
+            if (storedItem == item)
+            {
+                storedItem = null;
+            }
+        }
+
+        public void UnstoreItem()
+        {
+            if (storedItem)
+                storedItem.DetachSlot();
+        }
+
+        [SerializeField] protected MeshRenderer mesh;
+        [SerializeField] protected Material highlightMaterial;
+
+        protected Material unhighlightedMat;
+
+        public void _Highlight() //아이템 잡을 수 있으면 표현
+        {
+            if (mesh)
+            {
+                if (mesh.sharedMaterial != highlightMaterial)
+                {
+                    if (unhighlightedMat == null)
+                    {
+                        unhighlightedMat = mesh.sharedMaterial;
+                    }
+                    mesh.sharedMaterial = highlightMaterial;
+                }
+            }
+        }
+
+        public void _Unhighlight()
+        {
+            if (mesh)
+            {
+                if (mesh.sharedMaterial != unhighlightedMat)
+                {
+                    mesh.sharedMaterial = unhighlightedMat;
+                }
+            }
+
+        }
+
+        protected virtual void SetVisibility(bool visible) 
+        {
+            MeshRenderer tempMesh = GetComponent<MeshRenderer>();
+
+            if (tempMesh)
+            {
+                tempMesh.enabled = visible;
+            }
+        }
+        
+        protected virtual bool HasTag(Item item) //아이템 태그
+        {
+            if (AcceptedTags.Count == 0)
+            {
+                return true;
+            }
+
+            for (int i = 0; i < AcceptedTags.Count; i++)
+            {
+                if (item.HasTag(AcceptedTags[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public virtual bool ValidItem(Item potentialItem) //유효 아이템
+        {
+            if (potentialItem == null)
+            {
+                return false;
+            }
+
+            if (potentialItem.Size > sizeLimit)
+            {
+                return false;
+            }
+
+            if (!HasTag(potentialItem))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        protected virtual void FixedUpdate()
+        {
+            if (storedItem)
+            {
+                if (storedItem.transform.parent != offset)
+                {
+                    return;
+                }
+
+                if (poseItem)
+                {
+                    storedItem.Pose();
+                }
+            }
+        }
+
+        public virtual bool AddToStack(Item newStackable) //스택에 추가
+        {
+            if (!HasItem)
+            {
+                newStackable.Restrained = false;
+                newStackable.StoreOnSlot(this);
+                return false;
+            }
+
+            if (!newStackable.Stackable)
+                return false;
+
+            if (newStackable.StackableID != storedItem.StackableID)
+                return false;
+
+            if (StackIsFull)
+                return false;
+
+            newStackable.transform.SetParent(offset, true);
+            Vector3 newPosition = newStackable.PositionOffset;
+            newPosition.z -= (stackedItems.Count + 1) * 0.025f; 
+            newPosition.y -= (stackedItems.Count + 1) * 0.025f;
+            newStackable.transform.localPosition = newPosition;
+            newStackable.transform.localRotation = storedItem.transform.localRotation;
+            newStackable.SetPhysics(true, false, true, true);
+            newStackable.Restrained = true;
+            stackedItems.Add(newStackable);
+            return true;
+        }
+    }
+}
---
+++ Assets/Our Order Made Script/VR FPS/Weapons/Firearm Core/RecoilManager.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VrFps
+{
+    [System.Serializable]
+    public class RecoilManager //총기반동 매니저
+    {
+
+        //반동 포지션 로테이션
+        public Transform recoilPositionOffset;
+        public Transform recoilRotationOffset;
+
+        [System.Serializable]
+        public struct Recoil //리코일액시스에서 가져옴
+        {
+            public RecoilAxis x;
+            public RecoilAxis y;
+            public RecoilAxis z;
+        }
+
+
+        [SerializeField] protected Recoil translation;
+        [SerializeField] protected Recoil rotation;
+
+
+        //반동 관련
+        public void ClearRecoil()
+        {
+            translation.x.ResetAxis();
+            translation.y.ResetAxis();
+            translation.z.ResetAxis();
+            rotation.x.ResetAxis();
+            rotation.y.ResetAxis();
+            rotation.z.ResetAxis();
+        }
+
+        public void SetSelf(Item self)
+        {
+            SetRecoilSelf(ref translation, self);
+            SetRecoilSelf(ref rotation, self);
+        }
+
+        //스케일
+        public void ScaleRotation(float maxDamp, float incrementDamp, float decreaseTargetDamp, float increaseDamp, float decreaseDamp)
+        {
+            UnscaleRotation();
+            ScaleRecoil(maxDamp, incrementDamp, decreaseTargetDamp, increaseDamp, decreaseDamp, ref rotation);
+        }
+
+        public void ScaleTranslation(float maxDamp, float incrementDamp, float decreaseTargetDamp, float increaseDamp, float decreaseDamp)
+        {
+            UnscaleTranslation();
+            ScaleRecoil(maxDamp, incrementDamp, decreaseTargetDamp, increaseDamp, decreaseDamp, ref translation);
+        }
+
+        public void ScaleRecoil(float maxScale, float incrementScale, float decreaseTargetScale, float increaseScale, float decreaseScale,
+                                       ref Recoil recoil)
+        {
+            ScaleRecoilAxis(maxScale, incrementScale, decreaseTargetScale, increaseScale, decreaseScale, ref recoil.x);
+            ScaleRecoilAxis(maxScale, incrementScale, decreaseTargetScale, increaseScale, decreaseScale, ref recoil.y);
+            ScaleRecoilAxis(maxScale, incrementScale, decreaseTargetScale, increaseScale, decreaseScale, ref recoil.z);
+        }
+
+        void ScaleRecoilAxis(float maxScale, float incrementScale, float decreaseTargetScale, float increaseScale, float decreaseScale,
+                         ref RecoilAxis recoil)
+        {
+            recoil.twoHandMaxScale *= maxScale;
+            recoil.twoHandIncrementScale *= incrementScale;
+            recoil.twoHandDecreaseTargetScale *= decreaseTargetScale;
+            recoil.twoHandIncreaseScale *= increaseScale;
+            recoil.twoHandDecreaseScale *= decreaseScale;
+        }
+
+        public void UnscaleRotation()
+        {
+            UnscaleRecoil(ref rotation);
+        }
+
+        public void UnscaleTranslation()
+        {
+            UnscaleRecoil(ref translation);
+        }
+
+        public void UnscaleRecoil(ref Recoil recoil)
+        {
+            UnscaleRecoilAxis(ref recoil.x);
+            UnscaleRecoilAxis(ref recoil.y);
+            UnscaleRecoilAxis(ref recoil.z);
+        }
+
+        public void UnscaleRecoilAxis(ref RecoilAxis recoil)
+        {
+            recoil.SetInitial();
+        }
+
+        public void ApplyRecoil()
+        {
+            if (!recoilPositionOffset)
+            {
+                return;
+            }
+
+            ApplyRecoil(ref translation);
+            ApplyRecoil(ref rotation);
+
+            recoilPositionOffset.localPosition = new Vector3(translation.x.Current, translation.y.Current, translation.z.Current);
+            recoilRotationOffset.localRotation = Quaternion.Euler(new Vector3(rotation.x.Current, rotation.y.Current, rotation.z.Current));
+
+            DecreaseRecoil(ref translation);
+            DecreaseRecoil(ref rotation);
+        }
+
+        void ApplyRecoil(ref Recoil recoil)
+        {
+            recoil.x.ApplyRecoil();
+            recoil.y.ApplyRecoil();
+            recoil.z.ApplyRecoil();
+        }
+
+        public void IncreaseAllRecoil()
+        {
+            IncreaseRecoil(ref translation);
+            IncreaseRecoil(ref rotation);
+        }
+
+        void IncreaseRecoil(ref Recoil recoil)
+        {
+            recoil.x.IncreaseRecoil();
+            recoil.y.IncreaseRecoil();
+            recoil.z.IncreaseRecoil();
+        }
+
+        void DecreaseRecoil(ref Recoil recoil)
+        {
+            recoil.x.DecreaseRecoil();
+            recoil.y.DecreaseRecoil();
+            recoil.z.DecreaseRecoil();
+        }
+
+        void SetRecoilSelf(ref Recoil recoil, Item self)
+        {
+            recoil.x.self = self;
+            recoil.y.self = self;
+            recoil.z.self = self;
+
+            recoil.x.Initialize();
+            recoil.y.Initialize();
+            recoil.z.Initialize();
+        }
+    }
+}
