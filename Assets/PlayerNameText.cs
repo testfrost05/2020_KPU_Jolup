01d27195c6675c3b51a4caa135aea3ba6df23b58
sungyong <tjddyddl93@gmail.com>
2020-06-24 18:32:52 +0900

    name text add

---
+++ Assets/PlayerNameText.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerNameText : MonoBehaviour
+{
+    private Text nameText;
+    void Start()
+    {
+        nameText = GetComponent<Text>();
+
+        if (AuthManager.User != null)
+        {
+            nameText.text = $"Welcome! {AuthManager.User.Email}";
+        }
+        else
+        {
+            nameText.text = "ERROR : AuthManager.User == null";
+        }
+        
+    }
+
+    
+    
+}
