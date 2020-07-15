1df74d761ba79548eee77a1e5f384db6a00bd0db
sungyong <tjddyddl93@gmail.com>
2020-07-16 03:16:23 +0900

    서버 동기화 작업중

---
+++ Assets/Motion.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+namespace Com.Kpu.SimpleHostile
+{
+    public class Motion : MonoBehaviour
+    {
+        public float speed;
+
+        private Rigidbody rig;
+
+        private void Start()
+        {
+            Camera.main.enabled = false;
+            rig = GetComponent<Rigidbody>();
+        }
+
+        private void FixedUpdate()
+        {
+           
+            float t_hmove = Input.GetAxisRaw("Horizontal");
+            float t_vmove = Input.GetAxisRaw("Vertical");
+
+            Vector3 t_direction = new Vector3(t_hmove, 0, t_vmove);
+            t_direction.Normalize();
+
+            rig.velocity = transform.TransformDirection(t_direction) * speed * Time.deltaTime;
+        }
+    }
+}
