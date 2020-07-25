3ed6c5f52ba386c963d06682b09938a9d099ad3a
sungyong <tjddyddl93@gmail.com>
2020-07-26 02:51:29 +0900

    server for Ai changed again

--- Assets/EnemySpawnManager.cs
+++ Assets/EnemySpawnManager.cs
@@ -7,26 +7,9 @@ using UnityEngine.SceneManagement;
 
 public class EnemySpawnManager : MonoBehaviourPunCallbacks
 {
-    public static EnemySpawnManager Instance
-    {
-        get
-        {
-            if (Instance == null) instance = FindObjectOfType<EnemySpawnManager>();
-
-
-            return Instance;
-        }
-
-    }
-    private static EnemySpawnManager instance;
 
     public Transform[] spawnPositions;
     public GameObject playerPrefab1;
-
-
-
-
-
 
 
 
@@ -39,8 +22,7 @@ public class EnemySpawnManager : MonoBehaviourPunCallbacks
 
     public void SpawnPlayer()
     {
-        var localPlayerIndex = PhotonNetwork.LocalPlayer.ActorNumber - 1;
-        var spawnPosition = spawnPositions[localPlayerIndex % spawnPositions.Length];
+        Transform spawnPosition = spawnPositions[Random.Range(0,spawnPositions.Length)];
 
         PhotonNetwork.Instantiate(playerPrefab1.name, spawnPosition.position, spawnPosition.rotation);
 
@@ -49,14 +31,7 @@ public class EnemySpawnManager : MonoBehaviourPunCallbacks
 
 
     }
-    public override void OnLeftRoom()
-    {
-        SceneManager.LoadScene("MainMenu");
-    }
+    
+    
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 }
