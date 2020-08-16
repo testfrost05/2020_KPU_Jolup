cd4a0cc3bba8bddb7d9cb7174400f8daf2665507
sungyong <tjddyddl93@gmail.com>
2020-08-16 23:16:02 +0900

    target prefab add

--- Assets/Our Order Made Script/ETC/HealthManager.cs
+++ Assets/Our Order Made Script/ETC/HealthManager.cs
@@ -6,14 +6,37 @@ public class HealthManager : MonoBehaviour
 {
     public float hitPoint = 100f;
 
+
+    private Animator Target;
+
+    public void Start()
+    {
+        Target = GetComponent<Animator>();
+
+    }
+
+
+
+
+
     public void ApplyDamage(float damage)
     {
         hitPoint -= damage;
         if (hitPoint <= 0)
         {
             GameControl.instance.SlayTarget(); // 표적증가 추가 코드
-            Destroy(gameObject);
+            Die();
+            //Destroy(gameObject);
         }
+    }
+
+    public void Die()
+    {
+
+       
+        Target.SetBool("hit", true);
+    
+    
     }
 
 }
