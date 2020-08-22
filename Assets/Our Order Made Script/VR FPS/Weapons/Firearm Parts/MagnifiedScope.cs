531727ebbb02a3f162029fb5e1d71e5c4c2fd806
Lee Hyo Jun <u7i333@naver.com>
2020-08-22 10:35:59 +0900

    4Xscope add

---
+++ Assets/Our Order Made Script/VR FPS/Weapons/Firearm Parts/MagnifiedScope.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VrFps
+{
+    public class MagnifiedScope : Attachment
+    {
+
+        private Camera scopeCamera;
+        [SerializeField] protected GameObject scopeTexture;
+
+        protected override void Start() { base.Start(); scopeCamera = GetComponentInChildren<Camera>(); }
+
+        public override void Enable()
+        {
+            scopeCamera.enabled = true;
+            scopeTexture.SetActive(true);
+        }
+
+        public override void Disable()
+        {
+            scopeCamera.enabled = false;
+            scopeTexture.SetActive(false);
+        }
+    }
+}
